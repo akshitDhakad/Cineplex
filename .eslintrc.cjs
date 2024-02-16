32297d6d203b0414cd949adc8f305610c83e5138
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-02-16 20:51:20 +0530

    Update React and React DOM versions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' }, // Set to a valid ECMAScript version number or omit for latest
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
@@ -17,4 +17,28 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
+
+
+
+
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//     'plugin:react/jsx-runtime',
+//     'plugin:react-hooks/recommended',
+//   ],
+//   ignorePatterns: ['dist', '.eslintrc.cjs'],
+//   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+//   settings: { react: { version: '18.2' } },
+//   plugins: ['react-refresh'],
+//   rules: {
+//     'react-refresh/only-export-components': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//   },
+// }
